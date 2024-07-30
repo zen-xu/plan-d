bf504b57e67a3bcd2309a41a03600c8e05ec0f3a
ZhengYu, Xu <zen-xu@outlook.com>
2024-07-30 23:38:32 +0800

    feat: support use rich for better display (#8)
    * build: add extra dep rich
    
    * feat: impl rich
    
    * fix redirect stdio
    
    * fix redirect stdio
    
    * update
    
    * remove unused const
    
    * sort methods
    
    * highlight list lines
    
    * feat: use rich to print_list_lines
    
    * support disable rich by env var
    
    * ruff fix

--- plan_d/_internal/debugger.py
+++ plan_d/_internal/debugger.py
@@ -1,21 +1,31 @@
 from __future__ import annotations
 
+import io
+import linecache
 import os
 import subprocess
 import sys
+import traceback
 
 from contextlib import contextmanager
 from contextlib import nullcontext
 from contextlib import redirect_stderr
 from contextlib import redirect_stdout
+from termios import tcdrain
 from typing import TYPE_CHECKING
 from typing import TextIO
+
+import click
 
 from IPython.core.alias import Alias
 from IPython.terminal.debugger import TerminalPdb
 from IPython.terminal.interactiveshell import TerminalInteractiveShell
 from IPython.terminal.ptutils import IPythonPTLexer
+from madbg.communication import Piping
+from madbg.communication import receive_message
 from madbg.debugger import RemoteIPythonDebugger
+from madbg.tty_utils import PTY
+from madbg.utils import run_thread
 from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.filters import HasFocus
 from prompt_toolkit.filters import IsDone
@@ -30,6 +40,8 @@ if TYPE_CHECKING:
     from contextlib import AbstractContextManager
     from typing import Callable
 
+    from rich.console import Console
+
 
 def default_hello_message(ip: str, port: int) -> str:
     return f"RemotePdb session open at {ip}:{port}, use 'plan-d debug {ip} {port}' to connect..."
@@ -39,15 +51,16 @@ def default_accepted_message(client_address: str) -> str:
     return f"RemotePdb accepted connection from {client_address}."
 
 
-DEFAULT_PROMPT = "plan-d> "
-
-
 class RemoteDebugger(RemoteIPythonDebugger):
     def __init__(
         self,
         stdin: TextIO,
         stdout: TextIO,
         term_type: str | None,
+        height: int,
+        width: int,
+        console: Console | None = None,
+        syntax_theme: str = "ansi_dark",
         **extra_pt_session_options,
     ) -> None:
         # fix annoying `Warning: Input is not a terminal (fd=0)`
@@ -85,6 +98,41 @@ class RemoteDebugger(RemoteIPythonDebugger):
 
         self.use_rawinput = True
         self.done_callback = None
+        self.height = height
+        self.width = width
+        self.console = console
+        self.syntax_theme = syntax_theme
+
+    @classmethod
+    @contextmanager
+    def start(cls, sock_fd: int):
+        assert cls._get_current_instance() is None
+        term_data = receive_message(sock_fd)
+        term_attrs, term_type, term_size = (
+            term_data["term_attrs"],
+            term_data["term_type"],
+            term_data["term_size"],
+        )
+        with PTY.open() as pty:
+            pty.resize(term_size[0], term_size[1])
+            pty.set_tty_attrs(term_attrs)
+            pty.make_ctty()
+            piping = Piping({sock_fd: {pty.master_fd}, pty.master_fd: {sock_fd}})
+            with run_thread(piping.run):
+                slave_reader = os.fdopen(pty.slave_fd, "r")
+                slave_writer = os.fdopen(pty.slave_fd, "w")
+                try:
+                    instance = cls(slave_reader, slave_writer, term_type, *term_size)
+                    cls._set_current_instance(instance)
+                    yield instance
+                except Exception:
+                    print(traceback.format_exc(), file=slave_writer)
+                    raise
+                finally:
+                    cls._set_current_instance(None)
+                    print("Closing connection", file=slave_writer, flush=True)
+                    tcdrain(pty.slave_fd)
+                    slave_writer.close()
 
     @classmethod
     def connect_and_start(
@@ -116,6 +164,18 @@ class RemoteDebugger(RemoteIPythonDebugger):
             )
         return cls.start_from_new_connection(sock)
 
+    # =========== Magic funcs ===========
+
+    def do_pinfo(self, arg):
+        with self.dumb_term(), self.disable_console():
+            return super().do_pinfo(arg)
+
+    def do_pinfo2(self, arg):
+        with self.dumb_term(), self.disable_console():
+            return super().do_pinfo2(arg)
+
+    # =========== override ===========
+
     def onecmd(self, line: str) -> bool:
         """
         Invokes 'run_magic()' if the line starts with a '%'.
@@ -140,13 +200,86 @@ class RemoteDebugger(RemoteIPythonDebugger):
             self.error(f"{type(e).__qualname__} in onecmd({line!r}): {e}")
             return False
 
-    def do_pinfo(self, arg):
-        with self.dumb_term():
-            return super().do_pinfo(arg)
+    def error(self, msg: str, end="\n") -> None:
+        if self.console:
+            self.console.print(f"[danger]{msg}[/]", end=end)
+        else:
+            msg = click.style(msg, fg="red")
+            print(f"{msg}", file=self.stdout, end=end)
 
-    def do_pinfo2(self, arg):
-        with self.dumb_term():
-            return super().do_pinfo2(arg)
+    def message(self, *msgs, end="\n") -> None:
+        if self.console:
+            self.console.print(*msgs, end=end)
+        else:
+            print("\n".join(map(str, msgs)), file=self.stdout, end=end)
+
+    def print_stack_entry(self, frame_lineno, prompt_prefix="\n-> ", context=None):
+        if not self.console:
+            return super().print_stack_entry(frame_lineno, prompt_prefix, context)
+
+        import reprlib
+
+        from rich.syntax import Syntax
+
+        if context is None:
+            context = self.context
+        try:
+            context = int(context)
+            if context <= 0:
+                self.message("Context must be a positive integer")
+        except (TypeError, ValueError):
+            self.message("Context must be a positive integer")
+
+        frame, lineno = frame_lineno
+
+        # s = filename + '(' + `lineno` + ')'
+        filename = self.canonic(frame.f_code.co_filename)
+
+        func = str(frame.f_code.co_name) if frame.f_code.co_name else "<lambda>"
+
+        loc_frame = self._get_frame_locals(frame)
+        call = ""
+        if func != "?":
+            args = (
+                reprlib.repr(loc_frame["__args__"]) if "__args__" in loc_frame else "()"
+            )
+            call = f"{func}{args}"
+
+        self.message(f"🔴 {filename}([white]{lineno}[/])[cyan]{call}[/]")
+
+        start = lineno - 1 - context // 2
+        lines = linecache.getlines(filename)
+        start = min(start, len(lines) - context)
+        start = max(start, 0)
+        lines = lines[start : start + context]
+        code = Syntax(
+            "".join(lines),
+            lexer="python",
+            theme=self.syntax_theme,
+            line_numbers=True,
+            highlight_lines={lineno},
+            start_line=start + 1,
+            line_range=(0, context),
+        )
+
+        self.message(code, end="")
+
+    def print_list_lines(self, filename: str, first: int, last: int):
+        if not self.console:
+            return super().print_list_lines(filename, first, last)
+
+        from rich.syntax import Syntax
+
+        codes = Syntax.from_path(
+            filename,
+            line_numbers=True,
+            theme=self.syntax_theme,
+            line_range=(first, last),
+            highlight_lines={self.curframe.f_lineno},  # type: ignore[attr-defined]
+        )
+        self.message(codes)
+
+    # =========== methods ===========
 
     def run_magic(self, line) -> str:
         magic_name, arg, line = self.parseline(line)
@@ -188,7 +321,25 @@ class RemoteDebugger(RemoteIPythonDebugger):
 
     @contextmanager
     def redirect_stdio(self):
-        with redirect_stdout(self.stdout), redirect_stderr(self.stdout):
+        class StdoutWrapper(io.StringIO):
+            def __init__(self, debugger: RemoteDebugger):
+                self.debugger = debugger
+
+            def write(self, data):
+                self.debugger.message(data, end="")
+
+            def flush(self): ...
+
+        class StderrWrapper(io.StringIO):
+            def __init__(self, debugger: RemoteDebugger):
+                self.debugger = debugger
+
+            def write(self, data):
+                self.debugger.error(data, end="")
+
+            def flush(self): ...
+
+        with redirect_stdout(StdoutWrapper(self)), redirect_stderr(StderrWrapper(self)):
             yield
 
     @contextmanager
@@ -198,6 +349,20 @@ class RemoteDebugger(RemoteIPythonDebugger):
         os.environ["TERM"] = "dumb"
         yield
         os.environ["TERM"] = origin_term
+
+    @contextmanager
+    def disable_console(self):
+        origin_console = self.console
+        self.console = None
+        yield
+        self.console = origin_console
+
+    def resize(self, height: int, width: int):
+        self.height = height
+        self.width = width
+        if self.console:
+            self.console.height = height
+            self.console.width = width
 
 
 def call_magic_fn(alias: Alias, rest):
