eae88612a8964eaa1ea3cd1e2355f2620bc5c941
ZhengYu, Xu <zen-xu@outlook.com>
2024-08-01 12:25:07 +0800

    feat: impl launch_pland_on_exception

--- plan_d/__init__.py
+++ plan_d/__init__.py
@@ -7,5 +7,6 @@ __authors__ = [
 ]
 
 from ._internal.api import connect_to_debugger as connect_to_debugger
+from ._internal.api import launch_pland_on_exception as launch_pland_on_exception
 from ._internal.api import post_mortem as post_mortem
 from ._internal.api import set_trace as set_trace
